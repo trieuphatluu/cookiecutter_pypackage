4463c6ffa2ddbd32e82474638ef207b44f0ee70e
trieuphatluu <luubot2207@gmail.com>
2020-05-25 02:17:35 -0500

    Add utils and custom file info header

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}.py
@@ -1,1 +1,41 @@
-"""Main module."""
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+{{cookiecutter.project_slug}}.py
+{{cookiecutter.project_short_description}}
+-------------------------------------------------------------------------------------------------------------------
+
+Author: {{cookiecutter.full_name}}
+Contact: {{cookiecutter.email}}
+Created on: {% now 'utc-6', '%m/%d/%Y %H:%M:%S'%}
+'''
+# =================================================================================================================
+# IMPORT PACKAGES
+#%%
+from __future__ import print_function
+import os
+import inspect, sys
+import argparse
+#	File IO
+import json
+import yaml
+#	Data Analytics
+import pandas as pd
+import numpy as np
+import random
+#	Visualization Packages
+import matplotlib.pyplot as plt
+import skimage.io as skio
+#	Utilities
+from tqdm import tqdm
+import time
+from datetime import datetime
+#%%
+# =================================================================================================================
+# MAIN
+def main(**kwargs):
+	pass
+# =================================================================================================================
+# DEBUG
+if __name__ == '__main__':
+	main()
